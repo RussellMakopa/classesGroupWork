a2ae24fb03d39791c7c2e28b82b776d9c753885d
Abhinav Randive <arandive27@wooster.edu>
2025-02-14 13:35:42 -0500

    mul and div

---
+++ class2.py
@@ -0,0 +1,15 @@
+class Division:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def divide(self):
+        return self.num1 / self
+
+class Muliplication:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def multiply(self):
+        return self.num1 * self.num2
